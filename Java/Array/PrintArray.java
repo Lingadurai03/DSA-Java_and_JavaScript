1e0cec3f054697b18aadb4f5685925902a5011db
lingadurai03 <lingadurai033@gmail.com>
2024-11-24 09:11:03 +0530

    Add: print array

---
+++ Java/Array/PrintArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class PrintArray {
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
